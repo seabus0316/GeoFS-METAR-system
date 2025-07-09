acf92d2d4119f4b81f7ffadd66a59ec48d119d59
seabus0316 <yongyuchao@gmail.com>
2025-07-09 12:03:18 +0800

    Update geofs-metar.user.js

--- geofs-metar.user.js
+++ geofs-metar.user.js
@@ -5,7 +5,7 @@
 // @author       seabus + ChatGPT
 // @match        https://geo-fs.com/geofs.php*
 // @match        https://*.geo-fs.com/geofs.php*
-// @grant        none
+// @icon         https://i.ibb.co/wZ2SB149/Chat-GPT-Image-2025-6-30-08-31-37.png
 // ==/UserScript==
 
 (function () {
